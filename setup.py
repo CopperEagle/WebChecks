bbfa85103e5a2d7a129248f5a32acdf0f7889c2d
CopperEagle <137458753+CopperEagle@users.noreply.github.com>
2024-07-15 14:05:47 +0200

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webchecks",
-    version="0.1.2",
+    version="0.1.3",
     description='Web search and research tool',
     author='CopperEagle',
     license='BSD-3-Clause license',
